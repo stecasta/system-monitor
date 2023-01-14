6db70ef068e62f1452adc9fc832c240ac1098623
Stefano Castagnetta <steff@CVL-PF27F0ZQ-SC.localdomain>
2023-01-14 11:11:16 +0100

    Add os and kernel info

--- src/system.cpp
+++ src/system.cpp
@@ -1,17 +1,25 @@
+#include "system.h"
+
 #include <unistd.h>
+
 #include <cstddef>
 #include <set>
 #include <string>
 #include <vector>
 
+#include "linux_parser.h"
 #include "process.h"
 #include "processor.h"
-#include "system.h"
 
 using std::set;
 using std::size_t;
 using std::string;
 using std::vector;
+
+System::System() {
+  kernel_ = LinuxParser::Kernel();
+  operating_system_ = LinuxParser::OperatingSystem();
+}
 
 // TODO: Return the system's CPU
 Processor& System::Cpu() { return cpu_; }
@@ -20,13 +28,13 @@ Processor& System::Cpu() { return cpu_; }
 vector<Process>& System::Processes() { return processes_; }
 
 // TODO: Return the system's kernel identifier (string)
-std::string System::Kernel() { return string(); }
+std::string System::Kernel() { return kernel_; }
 
 // TODO: Return the system's memory utilization
 float System::MemoryUtilization() { return 0.0; }
 
 // TODO: Return the operating system name
-std::string System::OperatingSystem() { return string(); }
+std::string System::OperatingSystem() { return operating_system_; }
 
 // TODO: Return the number of processes actively running on the system
 int System::RunningProcesses() { return 0; }
