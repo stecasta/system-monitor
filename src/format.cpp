32e86c53e50678dbc3aa49a041c612fa9f2a461c
Stefano Castagnetta <steff@CVL-PF27F0ZQ-SC.localdomain>
2023-01-16 19:14:09 +0100

    Format time

--- src/format.cpp
+++ src/format.cpp
@@ -1,8 +1,23 @@
 #include "format.h"
 
+#include <chrono>
+#include <ctime>
+#include <iomanip>
 #include <string>
 
+using namespace std::chrono_literals;
 using std::string;
 
 // DONE: Complete this helper function
-string Format::ElapsedTime(long seconds) { return std::to_string(seconds); }
+// Using chrono library and std::put_time to format time
+string Format::ElapsedTime(long seconds) {
+  std::chrono::system_clock::time_point time;
+  std::chrono::milliseconds mil(seconds * 1000);
+  std::time_t now_time = std::chrono::system_clock::to_time_t(time);
+  time -= std::chrono::duration_cast<std::chrono::microseconds>(mil);
+  std::time_t start_time = std::chrono::system_clock::to_time_t(time);
+  std::time_t duration = now_time - start_time;
+  std::stringstream time_str;
+  time_str << std::put_time(std::localtime(&duration), "%H:%M:%S");
+  return time_str.str();
+}
