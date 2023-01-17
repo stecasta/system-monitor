fbc6d86e61009e19b7e4a44d397fa76b4e8711a7
Stefano Castagnetta <steff@CVL-PF27F0ZQ-SC.localdomain>
2023-01-17 09:57:55 +0100

    Bugfix: format time properly

--- src/format.cpp
+++ src/format.cpp
@@ -9,15 +9,11 @@ using namespace std::chrono_literals;
 using std::string;
 
 // DONE: Complete this helper function
-// Using chrono library and std::put_time to format time
 string Format::ElapsedTime(long seconds) {
-  std::chrono::system_clock::time_point time;
-  std::chrono::milliseconds mil(seconds * 1000);
-  std::time_t now_time = std::chrono::system_clock::to_time_t(time);
-  time -= std::chrono::duration_cast<std::chrono::microseconds>(mil);
-  std::time_t start_time = std::chrono::system_clock::to_time_t(time);
-  std::time_t duration = now_time - start_time;
-  std::stringstream time_str;
-  time_str << std::put_time(std::localtime(&duration), "%H:%M:%S");
-  return time_str.str();
+  long hours = seconds / 3600;
+  long minutes = (seconds % 3600) / 60;
+  long secs = seconds % 60;
+  char time[8];
+  sprintf(time, "%02d:%02d:%02d", hours, minutes, secs);
+  return time;
 }
